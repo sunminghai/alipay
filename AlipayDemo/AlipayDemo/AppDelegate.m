32531d4bae343158b34efe58530d398ae737041a
孙明海 <13317506550@163.com>
2016-02-29 20:30:15 +0800

    支付宝集成

--- AlipayDemo/AlipayDemo/AppDelegate.m
+++ AlipayDemo/AlipayDemo/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <AlipaySDK/AlipaySDK.h>
+
 
 @interface AppDelegate ()
 
@@ -14,6 +16,20 @@
 
 @implementation AppDelegate
 
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    //如果极简开发包不可用,会跳转支付宝钱包进行支付,需要将支付宝钱包的支付结果回传给开 发包
+    if ([url.host isEqualToString:@"com.sunminghai.ios"]) {
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url
+                                                  standbyCallback:^(NSDictionary *resultDic) {
+                                                      NSLog(@"result = %@",resultDic);
+                                                  }]; }
+    if ([url.host isEqualToString:@"platformapi"]){//支付宝钱包快登授权返回 authCode
+        [[AlipaySDK defaultService] processAuthResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES; }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
--- AlipayDemo/AlipayDemo/ViewController.m
+++ AlipayDemo/AlipayDemo/ViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <AlipaySDK/AlipaySDK.h>
+#import "Order.h"
+#import "DataSigner.h"
+#import "APAuthV2Info.h"
+
 
 @interface ViewController ()
 
@@ -26,9 +31,48 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    
+    /*=======================需要填写商户app申请的===================================*/
 
+    NSString *partner = @"";
+    NSString *seller = @"";
+    NSString *privateKey = @"";
+    
+    Order *order = [[Order alloc] init];
+    order.partner = partner;
+    order.seller = seller;
+    order.tradeNO = @"订单ID"; //订单ID(由商家□自□行制定)
+    order.productName = @"商品标题"; //商品标题
+    order.productDescription = @"商品标题"; //商品描述
+    order.amount = [NSString stringWithFormat:@"%.2f",6.66]; //商 品价格
+    
+    order.notifyURL = @"http://www.sunminghai.com"; //回调URL
+    
+    order.service = @"mobile.securitypay.pay";
+    order.paymentType = @"1";
+    order.inputCharset = @"utf-8";
+    order.itBPay = @"30m";
+    
+    //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
+    NSString *appScheme = @"com.sunminghai.ios";
+    //将商品信息拼接成字符串
+    NSString *orderSpec = [order description]; NSLog(@"orderSpec = %@",orderSpec);
+    
+    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循 RSA 签名规范, 并将签名字符串 base64 编码和 UrlEncode
+    id<DataSigner> signer = CreateRSADataSigner(privateKey);
+    NSString *signedString = [signer signString:orderSpec];
+    
+    //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    NSString *orderString = nil;
+    if (signedString != nil) {
+        orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
+                       orderSpec, signedString, @"RSA"];
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+            NSLog(@"reslut = %@",resultDic);
+        }];
+//        [tableView deselectRowAtIndexPath:indexPath animated:YES]; }
+    }
+}
 @end
